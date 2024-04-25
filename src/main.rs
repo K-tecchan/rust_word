6be386691bbb810c0d6381ab5576cbd42a76dbfd
k-tecchan <tecchanboy+pc@gmail.com>
2024-04-25 22:55:32 +0900

    記号も含むようにした

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
 mod cli;
 
 use clap::Parser;
-use rand::distributions::{Alphanumeric, DistString};
+use rand::distributions::{Alphanumeric, Distribution};
 
 use cli::{Cli, Type};
 
 fn main() {
     let args = Cli::parse();
-    let password = Alphanumeric.sample_string(&mut rand::thread_rng(), args.len);
-    // let password_symbols = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    let password_symbols = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    let mut password = String::new();
+
+    for _ in 0..args.len {
+        if rand::random::<u8>() < 192 {
+            password.push(Alphanumeric.sample(&mut rand::thread_rng()) as char);
+        } else {
+            password.push(
+                password_symbols
+                    .chars()
+                    .nth(rand::random::<usize>() % password_symbols.len())
+                    .unwrap(),
+            );
+        }
+    }
 
     match args.r#type {
         Type::Normal => println!("{}", password),
